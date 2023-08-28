11775bf491dff0e3c5e10cb70c60b12b042cd22e
Jan Hrastnik <jan.hrastnik2@gmail.com>
2023-08-28 23:55:41 +0200

    file fetching now works

--- fetch.py
+++ fetch.py
@@ -1,8 +1,30 @@
-import urllib.request
+from zipfile import ZipFile
+import requests
+import os.path
 
-# data source info: https://podatki.gov.si/dataset/surs0711335s
-# url to data source: https://pxweb.stat.si/SiStatData/Resources/PX/Databases/Data/0711335S.PX
+# data source info: https://podatki.gov.si/dataset/opozorilna-karta-poplav
+# url to data source: https://www.statika.evode.gov.si/fileadmin/vodkat/DRSV_OPKP_ZR_POPL.zip
 
-with urllib.request.urlopen("https://pxweb.stat.si/SiStatData/Resources/PX/Databases/Data/0711335S.PX") as F:
-    content = F.read()
-    print(content)
+def fetch_data() -> bool:
+    """returns true if data was successfully fetched, and false if something went wrong"""
+    print("Now fetching the required shapefile data")
+
+    if os.path.isfile("data.shp"):
+        # we already have the data file, in which case we can just return true now
+        print("Found existing 'data.shp' file!")
+        return True
+    
+    # unfortunately, the government site has an expired certificate...
+    data = requests.get("https://www.statika.evode.gov.si/fileadmin/vodkat/DRSV_OPKP_ZR_POPL.zip", verify=False)
+    if data.ok:
+        with open("data.zip", "wb") as F:
+            F.write(data.content)
+
+    with ZipFile("data.zip") as zip:
+        with zip.open("DRSV_OPKP_ZR_POPL.shp") as shapefile:
+            with open("data.shp", "wb") as F:
+                res = F.write(shapefile.read())
+                if res:
+                    print("'data.shp' has been successfully fetched and saved!")
+                    return True
+    return False
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fetch import fetch_data
+
 """
     Main file that runs the project.
     The project is split into 3 parts:
@@ -5,3 +7,8 @@
         2. Analysing the data
         3. Displaying the data
 """
+print("\n~~~~~~~~~~~~~~~~")
+print("~ The Project™ ~")
+print("~~~~~~~~~~~~~~~~\n")
+
+fetch_data()
